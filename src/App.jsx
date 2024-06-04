597389d247fbdad01fa4e42edc420725432a695a
mrchristopher4 <mrchristopher4@gmail.com>
2024-06-04 19:22:32 -0400

    First upload to remote

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,70 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({email:"", password:""});
+
+  function onInputChange(event) {
+    setFormData((oldFormData) => (
+      {...oldFormData, [event.target.name]: event.target.value})
+    );
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, formData.email, formData.password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 
   return (
     <>
-      <form className="login-form">
+      <form className="login-form" id="login-form">
         <div className="email-entry-container">
-          <label for="email-entry">Enter Email: </label>
-          <input type="email" id="email-entry" value="" className="email-input"></input>
+          <label htmlFor="email-entry">Enter Email: </label>
+          <input type="email" id="email-entry" name="email" value={formData.email} required
+          className="email-input" onChange={(event) => onInputChange(event)}></input>
         </div>
         <div className="password-entry-container">
-          <label for="password-entry">Enter Password: </label>
-          <input type="password" id="password-entry" value="" className="password-input"></input>
+          <label htmlFor="password-entry">Enter Password: </label>
+          <input type="password" id="password-entry" name="password" value={formData.password} required
+          className="password-input" onChange={(event) => onInputChange(event)}></input>
         </div>
-          <button type="submit" className="submit-btn">Submit Login Information</button>
+          <input value="Submit Login Information" type="submit" className="submit-btn" onClick={(event) => onSubmit(event)} />
       </form>
     </>
   )
 }
 
-export default App
+export default App;
